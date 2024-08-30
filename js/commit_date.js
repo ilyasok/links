dccdc52eb964fe853cc88e12b310439a4201ba31
m1sh3r <mish.fe2004@gmail.com>
2024-08-30 14:19:11 +0300

    переделка скрипта с коммитами

--- js/commit_date.js
+++ js/commit_date.js
@@ -14,16 +14,17 @@ async function getLastCommitDate() {
     }
     const commits = await response.json();
     if (commits.length === 0) {
-      document.getElementById("commit_date").textContent =
+      document.getElementById("commit_info").textContent =
         "Дата изменения\nвременно недоступна";
       return;
     }
     const lastCommitDate = new Date(commits[0].commit.author.date);
-    document.getElementById("commit_date").textContent =
-      "Дата последнего изменения:\n" + lastCommitDate.toLocaleString();
+    document.getElementById("commit_info").textContent =
+      "Обновлено:\n" + lastCommitDate.toLocaleString() +
+    "\n\nЧто нового: " + commits[0].commit.message;
   } catch (error) {
-    document.getElementById("commit_date").textContent =
-      "Ошибка получения даты!\n" + error;
+    document.getElementById("commit_info").textContent =
+      "Ошибка получения даты.\n" + error;
   }
 }
 
