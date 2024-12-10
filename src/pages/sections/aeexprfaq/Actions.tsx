575871df152d6988cd8c7a9773fa8b6dfb31f92f
m1sh3r <mish.fe2004@gmail.com>
2024-12-10 13:52:28 +0300

    избавляемся от лимита в миллион

--- src/pages/sections/aeexprfaq/Actions.tsx
+++ src/pages/sections/aeexprfaq/Actions.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import GithubUpdateInfo from "../../../components/features/GithubUpdateInfo";
+import DetailsSummary from "../../../components/DetailsSummary";
+import {AdditionInfo} from "../../../components/Additions";
+import {VideoFigure} from "../../../components/ContentFigure";
 
 const AEExprActions: React.FC = () => {
   return (
@@ -8,6 +11,36 @@ const AEExprActions: React.FC = () => {
       id="actions"
     >
       <GithubUpdateInfo filePath="src/pages/sections/aeexprfaq/Actions.tsx" />
+      <DetailsSummary title="Как мне сделать 'прыгучую' анимацию нужного параметра?">
+        <p>
+          {/* fixme: про bouncy текста или положения. можно привести в пример motion tools pro где это есть */}
+        </p>
+      </DetailsSummary>
+      <DetailsSummary title="Как сделать слайдер с значением больше миллиона?">
+        {/* fixme: написать!! */}
+        <p>
+          В <mark className="app">After Effects</mark> нет возможности поставить значение
+          в <mark className="plugin">Slider Control</mark> больше чем{" "}
+          <mark>1 000 000</mark> как в плюс, так и в минус (в итоге получается диапазон в
+          два миллиона). Если вы анимируете счётчик от нуля до какого-то значения, то
+          просто умножьте значение слайдера на нужный коэффициент, например на 10 или 25.
+        </p>
+        <VideoFigure
+          styleClass="figure_windows-dark"
+          videoSrc="images/aftereffects/limit_million.mp4"
+          caption="Умножение счётчика на нужный коэффициент"
+        />
+        <AdditionInfo>
+          Для избавления от плавающей запятой можно использовать функцию{" "}
+          <mark className="copy">Math.round</mark>.
+        </AdditionInfo>
+      </DetailsSummary>
+      <DetailsSummary title="Как сделать так, чтобы мой счётчик не дёргался при анимации числа?">
+        <p>{/* fixme: написать про monospace шрифт через поворот */}</p>
+      </DetailsSummary>
+      <DetailsSummary title="Как удалить выражения сразу в нескольких слоях?">
+        <p>{/* fixme: написать!! */}</p>
+      </DetailsSummary>
     </div>
   );
 };
