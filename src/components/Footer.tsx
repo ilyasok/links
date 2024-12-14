4f5a7a09b60bbc96feb5893f79f6f1de7339b416
m1sh3r <mish.fe2004@gmail.com>
2024-12-14 16:54:09 +0300

    добавлена модалка при нажатии на футер

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,7 @@
+import {Modal} from "antd";
 import React from "react";
+import {FlexibleLinks} from "./FlexibleLinksFaQ";
+import {GitHub, Mail} from "@mui/icons-material";
 
 interface FooterProps {
   title: string;
@@ -7,13 +10,95 @@ interface FooterProps {
 
 const Footer: React.FC<FooterProps> = ({title, initialYear}) => {
   const currentYear = new Date().getFullYear();
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <footer className="footer">
       <p className="footer__copyright">
-        {title} &copy; {initialYear} - {currentYear} {"// "}
-        <a href="https://t.me/m1sh3r">m1sh3r</a>
+        <button onClick={showModal}>
+          {title} &copy; {initialYear} - {currentYear} {"// "}
+          m1sh3r
+        </button>
       </p>
+      <Modal
+        open={isModalOpen}
+        closeIcon={null}
+        width={700}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={null}
+        centered
+      >
+        <div style={{display: "flex", flexDirection: "column", gap: "15px"}}>
+          <p
+            style={{
+              color: "var(--accent)",
+              fontWeight: 700,
+              fontSize: "20px",
+              fontFamily: '"Mulish", sans-serif',
+              textTransform: "uppercase",
+            }}
+          >
+            О сайте
+          </p>
+          <p>
+            Привет, я{" "}
+            <a
+              style={{
+                color: "var(--accent)",
+                fontWeight: 700,
+                textDecoration: "underline",
+              }}
+              href="https://t.me/m1sh3r"
+            >
+              Мишер
+            </a>
+            , создатель этой страницы. Идея сайта заключалась в том, чтобы минимизировать
+            надоедливые вопросы по программам Adobe и направить людей на альтернативные
+            чаты, если возникли вопросы по другим программам.{" "}
+            <i>Надеюсь, что с этой задачей я справился.</i>
+          </p>
+          <p>
+            Контент на сайте наполнялся благодаря команде администраторов и участников{" "}
+            <a
+              style={{
+                color: "var(--accent)",
+                fontWeight: 700,
+                textDecoration: "underline",
+              }}
+              href="https://t.me/joinchat/F1DdXtG9LephYWUy"
+            >
+              AEChat
+            </a>
+            .
+          </p>
+          <p style={{fontSize: "12px", opacity: "0.5"}}>
+            При копировании контента, пожалуйста, ссылайтесь на первоисточник, мы
+            старались не просто так. В каждом пункте можно скопировать ссылку, нажав на
+            кнопку справа.
+          </p>
+          <FlexibleLinks>
+            <a href="mailto:me@m1sh3r.ru">
+              <Mail />
+            </a>
+            <a href="https://github.com/aechat/links">
+              <GitHub />
+            </a>
+          </FlexibleLinks>
+        </div>
+      </Modal>
     </footer>
   );
 };
