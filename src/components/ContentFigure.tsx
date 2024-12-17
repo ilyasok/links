98c1d124a42c88534263ceb6123c68d966aad501
m1sh3r <mish.fe2004@gmail.com>
2024-12-17 23:42:05 +0300

    доработаны стили псевдоокон

--- src/components/ContentFigure.tsx
+++ src/components/ContentFigure.tsx
@@ -25,115 +25,164 @@ const ImageFigure: React.FC<ImageFigureProps> = ({
     styleClass === "figure_macos-light" || styleClass === "figure_macos-dark";
 
   return (
-    <figure className={styleClass}>
-      {isWindowsStyle && (
-        <div>
-          <div className="window-header">
-            <figcaption>{caption}</figcaption>
-            <div className="window-controls">
-              <div className="minimize">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 48 48"
-                >
-                  <path d="M 3.5 22.5 A 1.50015 1.50015 0 1 0 3.5 25.5 L 44.5 25.5 A 1.50015 1.50015 0 1 0 44.5 22.5 L 3.5 22.5 z"></path>
-                </svg>
-              </div>
-              <div className="maximize">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 48 48"
-                >
-                  <path d="M 11.5 6 C 8.4802259 6 6 8.4802259 6 11.5 L 6 36.5 C 6 39.519774 8.4802259 42 11.5 42 L 36.5 42 C 39.519774 42 42 39.519774 42 36.5 L 42 11.5 C 42 8.4802259 39.519774 6 36.5 6 L 11.5 6 z M 11.5 9 L 36.5 9 C 37.898226 9 39 10.101774 39 11.5 L 39 36.5 C 39 37.898226 37.898226 39 36.5 39 L 11.5 39 C 10.101774 39 9 37.898226 9 36.5 L 9 11.5 C 9 10.101774 10.101774 9 11.5 9 z"></path>
-                </svg>
+    <div className="figure_container">
+      <figure className={styleClass}>
+        {isWindowsStyle && (
+          <div>
+            <div className="window-header">
+              <figcaption>{caption}</figcaption>
+              <div className="window-controls">
+                <div className="minimize">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M5 20q-.425 0-.713-.288T4 19q0-.425.288-.713T5 18h14q.425 0 .713.288T20 19q0 .425-.288.713T19 20H5Z"
+                    />
+                  </svg>
+                </div>
+                <div className="maximize">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M6 20q-.825 0-1.413-.588T4 18V6q0-.825.588-1.413T6 4h12q.825 0 1.413.588T20 6v12q0 .825-.588 1.413T18 20H6Zm0-2h12V6H6v12ZM6 6v12V6Z"
+                    />
+                  </svg>
+                </div>
+                <div className="close">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275t.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7t-.7.275t-.7-.275z"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
+            <img
+              src={imgSrc}
+              alt={imgTitle}
+            />
           </div>
-          <img
-            src={imgSrc}
-            alt={imgTitle}
-          />
-        </div>
-      )}
-      {isMacOSStyle && (
-        <div>
-          <div className="window-header">
-            <div className="window-controls">
-              <div className="minimize"></div>
-              <div className="maximize"></div>
-              <div className="close"></div>
+        )}
+        {isMacOSStyle && (
+          <div>
+            <div className="window-header">
+              <div className="window-controls">
+                <div className="minimize"></div>
+                <div className="maximize"></div>
+                <div className="close"></div>
+              </div>
+              <figcaption>{caption}</figcaption>
             </div>
-            <figcaption>{caption}</figcaption>
+            <img
+              src={imgSrc}
+              alt={imgTitle}
+            />
           </div>
-          <img
-            src={imgSrc}
-            alt={imgTitle}
-          />
-        </div>
-      )}
-    </figure>
+        )}
+      </figure>
+    </div>
   );
 };
-
 const VideoFigure: React.FC<VideoFigureProps> = ({styleClass, videoSrc, caption}) => {
   const isWindowsStyle =
     styleClass === "figure_windows-light" || styleClass === "figure_windows-dark";
   const isMacOSStyle =
     styleClass === "figure_macos-light" || styleClass === "figure_macos-dark";
   return (
-    <figure className={styleClass}>
-      {isWindowsStyle && (
-        <div>
-          <div className="window-header">
-            <figcaption>{caption}</figcaption>
-            <div className="window-controls">
-              <div className="minimize">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 48 48"
-                >
-                  <path d="M 3.5 22.5 A 1.50015 1.50015 0 1 0 3.5 25.5 L 44.5 25.5 A 1.50015 1.50015 0 1 0 44.5 22.5 L 3.5 22.5 z"></path>
-                </svg>
-              </div>
-              <div className="maximize">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 48 48"
-                >
-                  <path d="M 11.5 6 C 8.4802259 6 6 8.4802259 6 11.5 L 6 36.5 C 6 39.519774 8.4802259 42 11.5 42 L 36.5 42 C 39.519774 42 42 39.519774 42 36.5 L 42 11.5 C 42 8.4802259 39.519774 6 36.5 6 L 11.5 6 z M 11.5 9 L 36.5 9 C 37.898226 9 39 10.101774 39 11.5 L 39 36.5 C 39 37.898226 37.898226 39 36.5 39 L 11.5 39 C 10.101774 39 9 37.898226 9 36.5 L 9 11.5 C 9 10.101774 10.101774 9 11.5 9 z"></path>
-                </svg>
+    <div className="figure_container">
+      <figure className={styleClass}>
+        {isWindowsStyle && (
+          <div>
+            <div className="window-header">
+              <figcaption>{caption}</figcaption>
+              <div className="window-controls">
+                <div className="minimize">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M5 20q-.425 0-.713-.288T4 19q0-.425.288-.713T5 18h14q.425 0 .713.288T20 19q0 .425-.288.713T19 20H5Z"
+                    />
+                  </svg>
+                </div>
+                <div className="maximize">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M6 20q-.825 0-1.413-.588T4 18V6q0-.825.588-1.413T6 4h12q.825 0 1.413.588T20 6v12q0 .825-.588 1.413T18 20H6Zm0-2h12V6H6v12ZM6 6v12V6Z"
+                    />
+                  </svg>
+                </div>
+                <div className="close">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275t.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7t-.7.275t-.7-.275z"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
+            <video
+              controls
+              loop
+              preload="metadata"
+            >
+              <source src={videoSrc} />
+            </video>
           </div>
-          <video
-            controls
-            loop
-            preload="metadata"
-          >
-            <source src={videoSrc} />
-          </video>
-        </div>
-      )}
-      {isMacOSStyle && (
-        <div>
-          <div className="window-header">
-            <div className="window-controls">
-              <div className="minimize"></div>
-              <div className="maximize"></div>
-              <div className="close"></div>
+        )}
+        {isMacOSStyle && (
+          <div>
+            <div className="window-header">
+              <div className="window-controls">
+                <div className="close"></div>
+                <div className="maximize"></div>
+                <div className="minimize"></div>
+              </div>
+              <figcaption>{caption}</figcaption>
             </div>
-            <figcaption>{caption}</figcaption>
+            <video
+              controls
+              loop
+              preload="metadata"
+            >
+              <source src={videoSrc} />
+            </video>
           </div>
-          <video
-            controls
-            loop
-            preload="metadata"
-          >
-            <source src={videoSrc} />
-          </video>
-        </div>
-      )}
-    </figure>
+        )}
+      </figure>
+    </div>
   );
 };
 
@@ -146,7 +195,9 @@ const YouTubeVideo: React.FC<{
   return (
     <figure className="figure_browser-youtube">
       <div className="window-header">
-        <figcaption>YouTube - {caption}</figcaption>
+        <figcaption>
+          <b>YouTube</b>: {caption}
+        </figcaption>
       </div>
       <iframe
         src={`https://www.youtube.com/embed/${id}`}
