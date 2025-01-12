65c8e5fbe002057b5c8e11a58eaeccbadc019c11
m1sh3r <mish.fe2004@gmail.com>
2025-01-12 14:00:16 +0300

    добавлена пасхалка в 404

--- src/pages/notFound.tsx
+++ src/pages/notFound.tsx
@@ -1,37 +1,73 @@
 import {motion} from "framer-motion";
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import {Helmet} from "react-helmet-async";
 import {Link} from "react-router-dom";
 
-const NotFound = () => (
-  <motion.main
-    className="not-found"
-    initial={{x: 100, opacity: 0}}
-    animate={{x: 0, opacity: 1}}
-    exit={{x: -50, opacity: 0}}
-    transition={{duration: 0.5, type: "spring", ease: [0.075, 0.82, 0.165, 1]}}
-  >
-    <Helmet>
-      <title>notfound@aechat</title>
-    </Helmet>
-    <div className="error-container">
-      <div className="error-modal">
-        <p className="error-modal-title">404 / Не найдено</p>
-        <p>Вы попали на несуществующую страницу, пожалуйста, перейдите на главную.</p>
-        <Link to="/">
-          <motion.div
-            className="error-modal-button"
-            whileHover={{
-              scale: 0.95,
-              transition: {duration: 0.5, ease: [0.075, 0.82, 0.165, 1]},
-            }}
-            whileTap={{scale: 0.9, opacity: 0.5}}
+const NotFound = () => {
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  useEffect(() => {
+    audioRef.current = new Audio("/files/404.mp3");
+
+    const handlePlayAudio = () => {
+      setTimeout(() => {
+        if (audioRef.current) {
+          audioRef.current.play().catch((error) => {
+            console.error("Ошибка воспроизведения аудио:", error);
+          });
+        }
+      }, 1000);
+    };
+
+    window.addEventListener("click", handlePlayAudio);
+
+    return () => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+        audioRef.current.currentTime = 0;
+      }
+      window.removeEventListener("click", handlePlayAudio);
+    };
+  }, []);
+
+  const handleLinkClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+  };
+
+  return (
+    <motion.main
+      className="not-found"
+      initial={{x: 100, opacity: 0}}
+      animate={{x: 0, opacity: 1}}
+      exit={{x: -50, opacity: 0}}
+      transition={{duration: 0.5, type: "spring", ease: [0.075, 0.82, 0.165, 1]}}
+    >
+      <Helmet>
+        <title>notfound@aechat</title>
+      </Helmet>
+      <p className="error-backtitle">404</p>
+      <div className="error-container">
+        <div className="error-modal">
+          <p className="error-modal-title">Страница не найдена</p>
+          <Link
+            to="/"
+            onClick={handleLinkClick}
           >
-            На главную
-          </motion.div>
-        </Link>
+            <motion.div
+              className="error-modal-button"
+              whileHover={{
+                scale: 0.95,
+                transition: {duration: 0.5, ease: [0.075, 0.82, 0.165, 1]},
+              }}
+              whileTap={{scale: 0.9, opacity: 0.5}}
+            >
+              На главную
+            </motion.div>
+          </Link>
+        </div>
       </div>
-    </div>
-  </motion.main>
-);
+    </motion.main>
+  );
+};
+
 export default NotFound;
