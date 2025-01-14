ded518a3aaaf06b1a3458ad9972e7b5eedbf2987
m1sh3r <mish.fe2004@gmail.com>
2025-01-14 15:13:54 +0300

    первоначальная версия раздела

--- src/pages/sections/psfaq/WhereFind.tsx
+++ src/pages/sections/psfaq/WhereFind.tsx
@@ -1,10 +1,293 @@
 import React from "react";
 import GithubUpdateInfo from "../../../components/features/GithubUpdateInfo";
+import {
+  AdditionDanger,
+  AdditionInfo,
+  AdditionWarning,
+} from "../../../components/Additions";
+import DetailsSummary from "../../../components/DetailsSummary";
+import {Divider} from "antd";
+import {ImageFigure} from "../../../components/ContentFigure";
 
 const PSWhereFind: React.FC = () => {
   return (
     <div className="faq-content">
       <GithubUpdateInfo filePath="src/pages/sections/psfaq/WhereFind.tsx" />
+      <DetailsSummary title="Где найти шаблоны, программы и плагины?">
+        <p>
+          На <a href="/">главной странице</a>, в закреплённом сообщении в чате{" "}
+          <a
+            href="https://t.me/joinchat/F1DdXtG9LephYWUy"
+            title="AEChat в Telegram"
+          >
+            AEChat
+          </a>{" "}
+          и в списке ниже вы всегда сможете найти ссылки на наши каналы с полезным
+          контентом.
+        </p>
+        <ul>
+          <li>
+            <a href="https://t.me/s/designworld">Design World</a> - канал с статичной
+            графикой, эффектами и кистями для <mark className="app">Photoshop</mark>,
+            которые в дальнейшем можно использовать в Ваших композициях{" "}
+            <mark className="app">After Effects</mark>.
+          </li>
+          <li>
+            <a href="https://t.me/s/aetemp">AETemp</a> и{" "}
+            <a href="https://t.me/s/premtemp">PremTemp</a> - каналы с переходами,
+            шаблонами, расширениями и прочими ништяками для ваших проектов в{" "}
+            <mark className="app">After Effects</mark> и{" "}
+            <mark className="app">Premiere Pro</mark>. Оттуда можно почерпнуть идеи для
+            ваших проектов в <mark className="app">Photoshop</mark>.
+          </li>
+        </ul>
+        <AdditionWarning>
+          Контент, предоставленный по ссылкам выше, не должен использоваться в
+          коммерческих целях!
+        </AdditionWarning>
+        <ul>
+          <li>
+            <a href="https://t.me/+Qd9xu7A4TeIwNzY6">склад стройматериалов</a> - канал с
+            творческими программами, утилитами и плагинами к различным программам по
+            демократичным ценам для пользователей компьютеров на базе Windows.
+          </li>
+          <li>
+            <a href="https://t.me/+ZqiaMfQRFgM4YmEy">Яблоневый сад</a> - аналогичный канал
+            с программами, как склад, но для пользователей устройств на macOS.
+          </li>
+        </ul>
+        <AdditionInfo>
+          Если у вас возникли проблемы с установкой программ или плагинов - прочтите
+          раздел <a href="#install-problems">Проблемы с установкой</a> или обратитесь к
+          нам в <a href="https://t.me/joinchat/F1DdXtG9LephYWUy">AEChat</a>.
+        </AdditionInfo>
+        <AdditionWarning>
+          При установке программ с пометкой <mark>репак от кролика</mark> из{" "}
+          <a href="https://t.me/+Qd9xu7A4TeIwNzY6">склада стройматериалов (Windows)</a> не
+          забывайте убирать галочку с пункта &quot;Реклама&quot; и отключать антивирус
+          перед установкой, иначе можете наткнуться на проблему{" "}
+          <mark>helper.exe не найден</mark>.
+        </AdditionWarning>
+        <AdditionDanger>
+          Устанавливая софт или плагины из каналов{" "}
+          <a href="https://t.me/s/aetemp">AETemp</a>,{" "}
+          <a href="https://t.me/+Qd9xu7A4TeIwNzY6">склад стройматериалов (Windows)</a> или{" "}
+          <a href="https://t.me/+ZqiaMfQRFgM4YmEy">Яблоневый сад (macOS)</a> - помните о
+          том, что вы принимаете риск и берёте ответственность за использование
+          нелегального ПО. Если вы так боитесь подцепить вирус, троян или похожее дерьмо,
+          то вам не место на пиратском море. Претензии людей по типу{" "}
+          <mark>&quot;ко-ко-ко, в складе/премтемпе адни выруси!!1!&quot;</mark>,{" "}
+          <mark>&quot;у миня антевырус ругается на hacktool!11&quot;</mark> и подобное мы
+          не можем принять всерьёз, потому что не знаем внятной причины доверять вбросу.
+        </AdditionDanger>
+      </DetailsSummary>
+      <DetailsSummary title="Как скачать нужный мне шаблон, если у меня есть на него ссылка?">
+        <p>
+          Если вы не нашли то, что вам нужно из ссылок, то у нас имеется бот{" "}
+          <a
+            href="https://t.me/HiStockBot?start=509060394"
+            title="Бот HiStock для покупки шаблонов в Telegram"
+          >
+            {" "}
+            HiStock в Telegram
+          </a>{" "}
+          для оперативной покупки шаблонов с платформ{" "}
+          <a
+            href="https://elements.envato.com"
+            title="Envato Elements"
+          >
+            Envato Elements
+          </a>
+          ,{" "}
+          <a
+            href="https://freepik.com/"
+            title="Freepik"
+          >
+            Freepik
+          </a>
+          ,{" "}
+          <a
+            href="https://motionarray.com/"
+            title="Motion Array"
+          >
+            Motion Array
+          </a>{" "}
+          и{" "}
+          <a
+            href="https://artlist.io/"
+            title="Artlist"
+          >
+            Artlist
+          </a>{" "}
+          по выгодным ценам.
+        </p>
+        <AdditionWarning>
+          Файлы из Videohive, Shutter Stock и других сервисов, которые не используют
+          подписочную модель распространения файлов, не поддерживаются ботом.
+        </AdditionWarning>
+      </DetailsSummary>
+      <DetailsSummary title="Где и как скачать нейро-фильтры в Photoshop?">
+        <p>
+          Вы, наверное, не раз натыкались на то, что вкладка{" "}
+          <mark className="ui">Neural Filters</mark> не доступна на &quot;народных
+          версиях&quot; <mark className="app">Photoshop</mark>. Она не активна по той
+          причине, что пользователь не вошёл в аккаунт Adobe.
+        </p>
+        <ImageFigure
+          styleClass="figure_windows-light"
+          imgSrc="images/photoshop/disabled_neural_filters.png"
+          imgTitle="Вкладка Neural Filters неактивна"
+          caption="Photoshop"
+        />
+        <p>
+          Чтобы активировать вкладку <mark className="ui">Neural Filters</mark> вам нужно
+          зарегистрировать (если у вас нет аккаунта Adobe) и войти в аккаунт. Для этого
+          перейдите в <mark className="ui">Help</mark> и нажмите на{" "}
+          <mark className="ui">Sign In</mark>. В открывшемся окне произведите вход в
+          аккаунт.
+        </p>
+        <AdditionInfo>
+          Не переживайте, псевдо-лицензия после входа в аккаунт не слетит. Если после
+          нажатия на <mark className="ui">Sign In</mark> вместо окна входа в аккаунт
+          появляется окно об отсутствии интернета, то просто попробуйте включить{" "}
+          <mark className="app">VPN</mark>. Обычно они игнорируют правила блокировки
+          доступа программе в интернет.
+        </AdditionInfo>
+        <ImageFigure
+          styleClass="figure_windows-light"
+          imgSrc="images/photoshop/help_sign_in.png"
+          imgTitle="Вход в аккаунт"
+          caption="Photoshop"
+        />
+        <p>
+          После входа в аккаунт - вкладка <mark className="ui">Neural Filters</mark> в
+          пункте <mark className="ui">Filter</mark> станет активной и вы сможете загрузить
+          нейро-фильтры, нажав на иконку загрузки рядом с ними.
+        </p>
+        <ImageFigure
+          styleClass="figure_windows-light"
+          imgSrc="images/photoshop/download_neural_filters.png"
+          imgTitle="Загружаем нейро-фильтры"
+          caption="Photoshop"
+        />
+        <p>
+          Но не тут то было, иногда загрузка нейро-фильтров сразу прерывается, не объясняя
+          причину. Чтобы это исправить - нужно внести несколько адресов в файл{" "}
+          <mark className="file">hosts</mark>. Для этого вам нужно перейти в{" "}
+          <mark className="path">C:\Windows\System32\drivers\etc</mark>, скопировать файл{" "}
+          <mark className="path">hosts</mark> в любое удобное место, открыть любой
+          текстовый редактор (например <mark className="app">Notepad++</mark> или{" "}
+          <mark className="app">VS Code</mark>) и внести строки чуть ниже в конец файла.
+          Затем сохраните отредактированный файл и перенесите его обратно в{" "}
+          <mark className="path">C:\Windows\System32\drivers\etc\hosts</mark> с заменой и
+          перезагрузите <mark className="app">Photoshop</mark>, если он у вас был открыт.
+        </p>
+        <code>
+          127.0.0.1 cc-api-data.adobe.io
+          <br />
+          127.0.0.1 ic.adobe.io
+          <br />
+          127.0.0.1 genuine.adobe.com
+          <br />
+          127.0.0.1 prod.adobegenuine.com
+          <br />
+          127.0.0.1 assets.adobedtm.com
+        </code>
+        <p>
+          После этого загрузка нейро-фильтров должна начаться нормально. Нейро-фильтры
+          обычно загружаются в папку{" "}
+          <mark className="path">
+            %AppData%\Adobe\UXP\PluginsStorage\PHSP\XX\Internal\com.adobe.nfp.gallery\PluginData
+          </mark>
+          , где <mark>XX</mark> - версия программы, указаная в первой части числа в{" "}
+          <mark className="ui">Help &gt; About Photoshop</mark>. Эта информация вам
+          пригодится, если вы хотите удалить файлы нейро-фильтров или распаковать их в
+          нужную папку из другого источника в интернете.
+        </p>
+      </DetailsSummary>
+      <DetailsSummary title="Где найти шрифт по имеющемуся фото?">
+        <p>
+          Вы можете использовать сервисом{" "}
+          <a
+            href="https://en.likefont.com"
+            title="Сервис по нахождению шрифтов LikeFont"
+          >
+            LikeFont
+          </a>{" "}
+          для определения шрифта по фотографии. Этот сервис способен распознавать шрифты
+          как на латинице, так и на кириллице.
+        </p>
+        <p>
+          Как начать им пользоваться? Всё просто: зайдите на сайт и загрузите своё
+          изображение с помощью проводника или <mark className="key">Ctrl + V</mark>.
+        </p>
+        <ImageFigure
+          styleClass="figure_windows-light"
+          imgSrc="images/likefont/likefont_mainpage.png"
+          imgTitle="Главная страница LikeFont"
+          caption="Главная страница LikeFont"
+        />
+        <AdditionDanger>
+          Учтите, что по умолчанию на странице стоит галка на{" "}
+          <mark className="ui">I agree to post it in Identification Community</mark>,
+          которая выкладывает ваш скриншот на общее обозрение. Если не хотите выкладывать
+          скриншот в сеть - уберите галочку.
+        </AdditionDanger>
+        <p>
+          Предположим, что на вход мы дали скриншот. Но вот незадача: он неконтрастный и
+          весь контент сливается. Что же делать?
+        </p>
+        <ImageFigure
+          styleClass="figure_windows-light"
+          imgSrc="images/likefont/psfaq_likefont_example.png"
+          imgTitle="Пример плохого исходника"
+          caption="Пример плохого исходника"
+        />
+        <p>
+          Для этого нужно предварительно обработать изображение. После загрузки
+          изображения сайт предлагает инструменты для обработки изображения, например{" "}
+          <mark className="ui">Threshold</mark>. Мы его повышаем до максимально возможного
+          значения, чтобы отделить текст от фона. Также есть инструменты для поворота,
+          отзеркаливания и наклона изображения при необходимости.
+        </p>
+        <ImageFigure
+          styleClass="figure_windows-light"
+          imgSrc="images/likefont/psfaq_likefont_fixed.png"
+          imgTitle="Изображение после обработки средствами сайта"
+          caption="Изображение после обработки средствами сайта"
+        />
+        <p>
+          Как мы видим, Threshold помог, но не сильно, так как фрактальный шум по яркости
+          совпал с яркостью текста. Но не унываем, у нас есть как минимум три чистых
+          буквы. После обработки пролистываем страницу ниже и мы видим несколько
+          изображений к которым нужно вписать букву.
+        </p>
+        <ImageFigure
+          styleClass="figure_windows-light"
+          imgSrc="images/likefont/psfaq_likefont_recognition.png"
+          imgTitle="Изображения к которым нужно вписать букву"
+          caption="Изображения к которым нужно вписать букву"
+        />
+        <p>
+          Если на таких изображениях нет букв или символа, то такие ячейки можно
+          пропустить. После того как вы вписали буквы, нажмите на{" "}
+          <mark className="ui">Identify Now</mark> и сервис начнёт распознавать шрифт, а
+          затем выдаст результат.
+        </p>
+        <AdditionInfo>
+          Чем больше вы впишете букв, тем лучше будет результат распознавания шрифта!
+        </AdditionInfo>
+        <ImageFigure
+          styleClass="figure_windows-light"
+          imgSrc="images/likefont/psfaq_likefont_result.png"
+          imgTitle="Результат распознавания"
+          caption="Результат распознавания"
+        />
+        <p>
+          В моём примере был использован шрифт <mark>Broadway</mark> и с его нахождением
+          сервис справился прекрасно.
+        </p>
+      </DetailsSummary>
     </div>
   );
 };
