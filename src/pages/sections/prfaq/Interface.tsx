5404502df4f6e9561616cd25ce0a24656630276e
m1sh3r <mish.fe2004@gmail.com>
2025-01-19 23:55:08 +0300

    написали про двойной язык интерфейса программы

--- src/pages/sections/prfaq/Interface.tsx
+++ src/pages/sections/prfaq/Interface.tsx
@@ -1,11 +1,38 @@
 import React from "react";
 import GithubUpdateInfo from "../../../components/features/GithubUpdateInfo";
 import DetailsSummary from "../../../components/DetailsSummary";
+import {AdditionDanger} from "../../../components/Additions";
+import {ImageFigure} from "../../../components/ContentFigure";
 
 const PRInterface: React.FC = () => {
   return (
     <div className="faq-content">
-      <GithubUpdateInfo filePath="src/pages/sections/prfaq/Interface.tsx" />{" "}
+      <GithubUpdateInfo filePath="src/pages/sections/prfaq/Interface.tsx" />
+      <DetailsSummary title="Как сделать двойной язык интерфейса?">
+        <p>
+          Если вы не хотите кардинально менять язык программы, то вы можете установить
+          &quot;двойной язык&quot; интерфейса. Для этого откройте консоль с помощью
+          комбинации клавиш <mark className="key">Ctrl + F12</mark> и переключитесь на
+          режим отображения <mark className="ui">Debug Database View</mark>. Далее найдите
+          пункты <mark className="ui">ApplicationLanguage</mark> и{" "}
+          <mark className="ui">ApplicationLanguageBilingual</mark> и укажите значения{" "}
+          <mark className="copy">ru_RU</mark> и <mark className="copy">true</mark>{" "}
+          соответственно. После этого перезагрузите программу и теперь во многих пунктах
+          интерфейса у вас будет русский язык и в скобках будет эквивалент на английском
+          языке.
+        </p>
+        <ImageFigure
+          styleClass="figure_windows-dark"
+          imgSrc="images/premierepro/set_double_language.png"
+          imgTitle="Установка двойного языка"
+          caption="Premiere Pro"
+        />
+        <AdditionDanger>
+          Не рекомендуется использовать двойной язык на постоянной основе, так как
+          основной язык программы останется русским, а соответственно баги русской
+          локализации <mark className="app">Premiere Pro</mark> никуда не денутся от вас.
+        </AdditionDanger>
+      </DetailsSummary>
       <DetailsSummary title="Как изменить вид отсчёта времени на таймлайне?">
         <p>{/* FIXME: написать!! */}</p>
       </DetailsSummary>
