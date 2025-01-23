98d7b5c8a88becf821396be10bb3eaf90f7bc40e
m1sh3r <mish.fe2004@gmail.com>
2025-01-23 23:16:18 +0300

    добавлены пустые пункты

--- src/pages/sections/psfaq/Export.tsx
+++ src/pages/sections/psfaq/Export.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import GithubUpdateInfo from "../../../components/features/GithubUpdateInfo";
+import DetailsSummary from "../../../components/DetailsSummary";
 
 const PSExport: React.FC = () => {
   return (
     <div className="faq-content">
       <GithubUpdateInfo filePath="src/pages/sections/psfaq/Export.tsx" />
+      <DetailsSummary title="Какие параметры экспорта нужно установить для загрузки в социальные сети?">
+        <p>{/* FIXME: */}</p>
+      </DetailsSummary>
+      <DetailsSummary title="Как сохранить слои отдельными файлами?">
+        <p>{/* FIXME: */}</p>
+      </DetailsSummary>
+      <DetailsSummary title="Как сохранить последовательность фото в .gif?">
+        <p>{/* FIXME: */}</p>
+      </DetailsSummary>
     </div>
   );
 };
