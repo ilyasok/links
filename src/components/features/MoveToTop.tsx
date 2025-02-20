8c1c2eb2fd11b6a7fc8dfaaf3335b3d665cb86a5
m1sh3r <mish.fe2004@gmail.com>
2025-02-20 18:25:03 +0300

    добавлен сброс якоря при нажатии

--- src/components/features/MoveToTop.tsx
+++ src/components/features/MoveToTop.tsx
@@ -7,6 +7,7 @@ export const MoveToTop = () => {
       top: 0,
       behavior: "smooth",
     });
+    window.history.replaceState({}, '', window.location.pathname);
   };
 
   return (
@@ -15,3 +16,4 @@ export const MoveToTop = () => {
     </button>
   );
 };
+
